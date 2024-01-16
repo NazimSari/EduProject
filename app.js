96095082752f4787ded5c64c258a65625a9c3f99
Nazim <yasarnazimsari@gmail.com>
2024-01-16 16:12:12 +0300

    routes mongodb courses model

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const pageRoute = require("./routes/pageRoute");
+const courseRoute = require("./routes/courseRoute");
 
 const app = express();
+
+//database
+mongoose.connect("mongodb://127.0.0.1:27017/smartedu-db").then(() => {
+  console.log("Database connected");
+});
 
 //template engine
 app.set("view engine", "ejs");
@@ -9,17 +17,8 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 //Routes
-app.get("/", (req, res) => {
-  res.status(200).render("index", {
-    page_name: "index",
-  });
-});
-
-app.get("/about", (req, res) => {
-  res.status(200).render("about", {
-    page_name: "about",
-  });
-});
+app.use("/", pageRoute);
+app.use("/courses", courseRoute);
 
 const port = 3000;
 app.listen(port, () => {
