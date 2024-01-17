21ba698f7d1e148e282d6230b9323cecc68a7db6
Nazim <yasarnazimsari@gmail.com>
2024-01-17 12:53:58 +0300

    categories created

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,4 +1,5 @@
 const Course = require("../models/Course");
+const Category = require("../models/Category");
 
 const createCourse = async (req, res) => {
   try {
@@ -17,9 +18,19 @@ const createCourse = async (req, res) => {
 
 const getAllCourses = async (req, res) => {
   try {
-    const courses = await Course.find();
+    const categorySlug = req.query.categories;
+    const category = await Category.findOne({ slug: categorySlug });
+    let filter = {};
+    if (category) {
+      filter = { category: category._id };
+    }
+    const courses = await Course.find(filter);
+
+    const categories = await Category.find();
+
     res.status(200).render("courses", {
       courses,
+      categories,
       page_name: "courses",
     });
   } catch (error) {
