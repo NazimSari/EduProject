2a87454decb1d1f926e9ab132a07dfabadf0cfb1
Nazim <yasarnazimsari@gmail.com>
2024-01-18 22:25:31 +0300

    contact form

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,3 +1,8 @@
+const nodemailer = require("nodemailer");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const getIndexPage = (req, res) => {
   console.log(req.session.userID);
   res.status(200).render("index", {
@@ -21,10 +26,52 @@ const getLoginPage = (req, res) => {
     page_name: "login",
   });
 };
+const getContactPage = (req, res) => {
+  res.status(200).render("contact", {
+    page_name: "contact",
+  });
+};
+const sendEmail = async (req, res) => {
+  const outputMessage = `
+    <h1>Mail Details</h1>
+    <ul>
+      <li>Name: ${req.body.name}</li>
+      <li>Email: ${req.body.email}</li>
+    </ul>
+    <h1>Message</h1>
+    <p>${req.body.message}</p>
+  `;
 
+  const transporter = nodemailer.createTransport({
+    host: process.env.NODEMAILER_SERVICE,
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.NODEMAILER_USER, //gmail account
+      pass: process.env.NODEMAILER_PASS, //gmail password
+    },
+  });
+
+  // async..await is not allowed in global scope, must use a wrapper
+  async function main() {
+    // send mail with defined transport object
+    const info = await transporter.sendMail({
+      from: '"Smart EDU contact" <digitalistway@gmail.com>', // sender address
+      to: "yasarnazimsari@gmail.com", // list of receivers
+      subject: "Smart EDU Contact Form New Message", // Subject line
+      html: outputMessage, // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+  }
+  res.status(200).redirect("contact");
+};
 module.exports = {
   getAboutPage,
   getIndexPage,
   getRegisterPage,
   getLoginPage,
+  getContactPage,
+  sendEmail,
 };
