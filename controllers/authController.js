dfa269615a7b3bc37a4194300e51270e1799146c
Nazim <yasarnazimsari@gmail.com>
2024-01-19 15:47:34 +0300

    kullanıcı kayıt doğrulama

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,16 +2,18 @@ const bcrypt = require("bcrypt");
 const User = require("../models/User");
 const Category = require("../models/Category");
 const Course = require("../models/Course");
-
+const { validationResult } = require("express-validator");
 const createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
     res.status(201).redirect("/login");
   } catch (error) {
-    res.status(400).json({
-      status: "fail",
-      error,
-    });
+    const errors = validationResult(req);
+
+    for (let i = 0; i < errors.array().length; i++) {
+      req.flash("error", `${errors.array()[i].msg}`);
+    }
+    res.status(201).redirect("/register");
   }
 };
 
@@ -20,30 +22,26 @@ const loginUser = async (req, res) => {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
 
-    if (!user) {
-      return res.status(401).json({
-        status: "fail",
-        error: "Invalid email or password",
-      });
+    if (user) {
+      const passwordMatch = await bcrypt.compare(password, user.password);
+
+      if (passwordMatch) {
+        // USER SESSION
+        req.session.userID = user._id;
+        return res.status(200).redirect("/users/dashboard");
+      } else {
+        req.flash("error", "Your password or email is not correct!");
+        return res.status(400).redirect("/login");
+      }
+    } else {
+      req.flash("error", "User is not found!");
+      return res.status(400).redirect("/login");
     }
-
-    const passwordMatch = await bcrypt.compare(password, user.password);
-
-    if (!passwordMatch) {
-      return res.status(401).json({
-        status: "fail",
-        error: "Invalid email or password",
-      });
-    }
-
-    // USER SESSION
-    req.session.userID = user._id;
-    res.status(200).redirect("/users/dashboard");
   } catch (error) {
     console.error(error);
-    res.status(500).json({
+    return res.status(400).json({
       status: "fail",
-      error: "Internal Server Error",
+      error,
     });
   }
 };
